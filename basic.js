bcac6e0f3b401e166ece554b107b43356a36e5e6
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-09 08:49:55 +0600

    adding

---
+++ basic.js
@@ -0,0 +1,10 @@
+"use strict";
+//primitives
+//basic properties
+let age;
+age = 12;
+let userName;
+userName = 'shakil hossain';
+let isInstalled;
+isInstalled = false;
+// let's write complex type
---
+++ basic.ts
@@ -0,0 +1,17 @@
+//primitives
+
+//basic properties
+
+let age: number;
+
+age = 12;
+
+let userName : string;
+
+userName = 'shakil hossain';
+
+let isInstalled : boolean;
+
+isInstalled = false;
+
+// let's write complex type
