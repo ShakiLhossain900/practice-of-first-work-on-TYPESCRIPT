94369638db35981df4764e077091a9c90a98b8c5
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-12 08:06:46 +0600

    adidng

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Todos: React.FC<{}> = (props) => {    //props is object type   //generic type FC
+const Todos: React.FC<{items: string[]}> = (props) => {    //props is object type   //generic type FC
     return (
         <ul>
-          {props.}
+          {props.items.map(item =><li key={item}>{item}</li>)}
         </ul>
     );
 };
