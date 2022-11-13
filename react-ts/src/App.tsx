3779c8ab822af8b7f016e732e3b9bb825614e7ea
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-11-13 18:54:53 +0600

    adding

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -3,12 +3,14 @@ import Todo from './models/todo';
 
 import './App.css';
 
+
+
 function App() {
   const todos =[
     new Todo('Learn react'),
     new Todo('Leran TypeScript')
   ]
-  
+
   return (
     <div >
    <Todos items={todos}/>
